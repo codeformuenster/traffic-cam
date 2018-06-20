c8219271c25352b4003a2c853dcb6d7c86bc8c5e
Thorben Jensen <jensen.thorben@googlemail.com>
2018-06-20 22:06:12 +0200

    milestone: lightnet demo

---
+++ src/02_yolo.py
@@ -0,0 +1,18 @@
+'''Yolo object detection, labels from COCO dataset.'''
+
+import lightnet
+from collections import Counter
+
+# load yolo model
+model = lightnet.load('yolo')
+
+# object detection
+image_file = 'data/2018-03-20T19:27:26+0000.png'
+image = lightnet.Image.from_bytes(
+    open(image_file, 'rb').read())
+boxes = model(image, thresh=0.1)
+
+# count boxes
+objects = [e[1] for e in boxes]
+obj_count = dict(Counter(objects))
+print('Objects found in image: {}'.format(obj_count))
