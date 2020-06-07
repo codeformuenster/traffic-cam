51784773322b4419e43f0b7518805b3eb9f20387
Thorben Jensen <jensen.thorben@gmail.com>
2020-06-07 14:09:03 +0200

    parse command line arguments for script

--- bin/download_images.py
+++ bin/download_images.py
@@ -10,20 +10,23 @@ from traffic_cam import io
 logging.basicConfig(level=logging.INFO)
 
 # config
-# parser = argparse.ArgumentParser()
-# parser.add_argument("sleep", help="how many seconds to sleep between downloads",
-#                     type=int, default=15)
-# args = parser.parse_args()
-# print(args.square**2)
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "n_images", help="number of images to download", type=int, default=15
+)
+parser.add_argument(
+    "sleep", help="number of seconds to sleep between downloads", type=int, default=15
+)
+args = parser.parse_args()
 
-SECONDS_SLEEP: int = 15
-N_IMAGES: int = 4
+logging.info(f"args.n_images: {args.n_images}")
+logging.info(f"args.sleep: {args.sleep}")
 
 # download images
-for i in range(N_IMAGES):
-    logging.info(f"Download image {i + 1} of {N_IMAGES}...")
+for i in range(args.n_images):
+    logging.info(f"Download image {i + 1} of {args.n_images}...")
     io.download_frame()
-    logging.info(f"Sleeping {SECONDS_SLEEP} seconds...")
-    time.sleep(SECONDS_SLEEP)
+    logging.info(f"Sleeping {args.sleep} seconds...")
+    time.sleep(args.sleep)
 
 logging.info("Done.")
