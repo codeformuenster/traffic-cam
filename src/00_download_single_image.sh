1eb44d2ab57893da053477c4148f43d959065911
Thorben Jensen <jensen.thorben@gmail.com>
2020-06-07 13:12:32 +0200

    remove replaced download scripts

--- src/00_download_single_image.sh
+++
@@ -1,7 +0,0 @@
-timestamp=$(date -u "+%Y-%m-%d-%H:%M:%S.%N:%S3+00:00")
-
-## downloading sample images.
-ffmpeg \
-    -i https://56f2a99952126.streamlock.net/833/default.stream/playlist.m3u8 \
-    -vframes 1 \
-    data/image_$timestamp.jpg
