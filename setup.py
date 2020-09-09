4ba55111fa9421674538ff51d9c7d4db41ccc80d
Thorben Jensen <jensen.thorben@gmail.com>
2020-09-09 21:23:47 +0200

    migrate dependency management from conda to pip-tools

--- setup.py
+++ setup.py
@@ -8,7 +8,17 @@ setup(
     license="MIT",
     packages=find_packages(),
     scripts=glob("bin/*"),
-    install_requires=["tensorflow==2.1.1",
-                      "Pillow"],
-    extras_require={"dev": ["black", "flake8", "rope"]},
+    install_requires=[
+        "tensorflow==2.1.1",
+        "Pillow",
+        "opencv-python",
+        "numpy",
+        # "yolo34py",  # requires pkg-config, e.g. 'sudo apt install pkg-config#'
+        "matplotlib",
+        # dev
+        "black",
+        "flake8",
+        "jupyter",
+        "rope",
+    ],
 )
