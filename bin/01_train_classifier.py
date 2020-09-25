e4cf936b2fa2b4d48c5542a23d0951be604ec98f
Thorben Jensen <jensen.thorben@gmail.com>
2020-09-25 16:52:21 +0200

    read train and validation data from same file tree

--- bin/01_train_classifier.py
+++ bin/01_train_classifier.py
@@ -2,13 +2,39 @@
 """ Train image classifier to distinguish camera positions. """
 
 import json
+from tensorflow.keras.applications.mobilenet import preprocess_input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 from traffic_cam import classifier, paths
 
 
+SEED = 42
+VALIDATION_SPLIT = 0.3
+
 # data generators for training and validation
-train_generator = classifier.get_image_datagen(folder=paths.TRAIN_DIR, batch_size=16)
-valid_generator = classifier.get_image_datagen(folder=paths.VALID_DIR, batch_size=16)
+datagen = datagen = ImageDataGenerator(
+    preprocessing_function=preprocess_input, validation_split=VALIDATION_SPLIT
+)
+train_generator = datagen.flow_from_directory(
+    paths.TRAIN_DIR,
+    seed=SEED,
+    target_size=(224, 224),
+    color_mode="rgb",
+    batch_size=16,
+    class_mode="categorical",
+    shuffle=True,
+    subset="training",
+)
+valid_generator = datagen.flow_from_directory(
+    paths.TRAIN_DIR,
+    target_size=(224, 224),
+    color_mode="rgb",
+    batch_size=16,
+    class_mode="categorical",
+    shuffle=True,
+    subset="validation",
+)
+
 
 # train model
 model = classifier.get_classifier(n_classes=5)
