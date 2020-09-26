9e5e854986c17cac1bc0fb372c723d7c1460e31d
Thorben Jensen <jensen.thorben@gmail.com>
2020-09-26 11:20:47 +0200

    draft script to send counts to api

---
+++ bin/05_count_to_api.py
@@ -0,0 +1,20 @@
+""" Count persons on Prinzipalmarkt and publish to API. """
+
+import requests
+
+
+# download image
+# TODO
+
+# classify image location
+# TODO
+# if image location not useful, wait and download new image
+# TODO
+
+# count persons on image
+# TODO
+
+# collect addiational information for API
+# TODO
+# send count to API
+# TODO
